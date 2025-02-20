8e718ecbf4384f8b08f5d9d6f55f511f49d1ccbc
Duke Lee <minhduc8a2.1@gmail.com>
2025-02-20 16:38:32 +0700

    add users option to main program

--- cobol/main_program.cbl
+++ cobol/main_program.cbl
@@ -1,18 +1,20 @@
        IDENTIFICATION DIVISION.
        PROGRAM-ID. main_program.
+
        DATA DIVISION.
        WORKING-STORAGE SECTION.
        01 CHOICE PIC 9 VALUE 0.
+
        PROCEDURE DIVISION.
-           
        MAIN-PROGRAM.
-           PERFORM UNTIL CHOICE = 6
+           PERFORM UNTIL CHOICE = 7
                DISPLAY "1. Init database"
-               DISPLAY "2. Show users"
-               DISPLAY "3. Find user"
-               DISPLAY "4. Edit user"
-               DISPLAY "5. Delete user"
-               DISPLAY "6. Exit"
+               DISPLAY "2. Add user"
+               DISPLAY "3. Show users"
+               DISPLAY "4. Find user"
+               DISPLAY "5. Edit user"
+               DISPLAY "6. Delete user"
+               DISPLAY "7. Exit"
                ACCEPT CHOICE
                EVALUATE CHOICE
                    WHEN 1
@@ -27,7 +29,7 @@
                    WHEN 5
                        CALL "DELETE_USER"
                    WHEN 6
-                       MOVE 6 TO CHOICE
+                       MOVE 7 TO CHOICE
                    WHEN OTHER
                        DISPLAY "Invalid Choice"
                END-EVALUATE
