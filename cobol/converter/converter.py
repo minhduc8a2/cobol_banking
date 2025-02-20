83d9490f495421699d6853cf32110ecd31cc1d5e
Duke Lee <minhduc8a2.1@gmail.com>
2025-02-20 17:04:35 +0700

    update readme.md

--- cobol/converter/converter.py
+++ cobol/converter/converter.py
@@ -1,8 +1,10 @@
 import pandas as pd
 import os
 current_folder = os.getcwd()
+parent_folder = os.path.dirname(current_folder)
 for file_name in os.listdir(current_folder):
     if file_name.endswith(".xlsx"):
         df = pd.read_excel(file_name)
-        df.to_csv(file_name.replace(".xlsx", ".csv"), index=False)
+        output_file = os.path.join(parent_folder, file_name.replace(".xlsx", ".csv"))
+        df.to_csv(output_file, index=False)
 
--- readme.md
+++ readme.md
@@ -1,9 +1,12 @@
 # Cobol
 ## Convert excel files to csv files
+At folder "converter"
 1. `python -m venv venv`
-2. `pip install pandas openpyxl`
-3. `python converter.py`
+2. `venv\Scripts\activate`
+3. `pip install pandas openpyxl`
+4. `python converter.py`
 ## Run Cobol program
+At folder "cobol"
 1. `cobc -m read_balance.cbl`
 2. `cobc -m read_user.cbl`
 2. `cobc -m show_users.cbl`
