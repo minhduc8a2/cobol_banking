ef0aa2763acfbf87d33a6f837491d076fd2404fa
Uriel Ofir <urielofir@gmail.com>
2025-02-26 08:34:11 +0200

    Transfer money with show user

--- cobol/transfer_money.cbl
+++ cobol/transfer_money.cbl
@@ -1,7 +1,7 @@
        >>SOURCE FORMAT FREE
        IDENTIFICATION DIVISION.
        PROGRAM-ID. TRANSFER_MONEY.
-       
+
        ENVIRONMENT DIVISION.
        INPUT-OUTPUT SECTION.
        FILE-CONTROL.
@@ -13,23 +13,66 @@
                ORGANIZATION IS INDEXED
                ACCESS MODE IS RANDOM
                RECORD KEY IS USER-REC-ID.
-               
+
        DATA DIVISION.
        FILE SECTION.
        FD USER-DB-FILE.
-       01 USER-DB-RECORD.
-           05 USER-REC-ID    PIC 9(10).
-           05 USER-REC-NAME  PIC X(30).
-           05 USER-REC-DOB   PIC X(10).  *> Format: YYYY-MM-DD
-           
-       FD BALANCE-DB-FILE.
-       01 BALANCE-DB-RECORD.
-           05 BALANCE-REC-ID     PIC 9(10).
-           05 BALANCE-REC-BALANCE PIC 9(10)V99.
+       COPY "user_record.cpy".
 
+       FD BALANCE-DB-FILE.
+       COPY "balance_record.cpy".
+
+
+       WORKING-STORAGE SECTION.
+       01 WS-USER-ID             PIC 9(10).
+       01 TRANSFER-TO-ID      PIC 9(10).
+       01 TRANSFER-AMOUNT     PIC 9(10)V99.
+       01 DOES-HAVE-THE-MONEY PIC 9 VALUE 0.
+       01 USER-DB-FILE-STATUS    PIC XX.
+       01 BALANCE-DB-FILE-STATUS PIC XX.
+       01 IS-VALID         PIC 9 VALUE 1.
 
        PROCEDURE DIVISION.
-           DISPLAY "----------------------"
+           DISPLAY "----------------------------"
            DISPLAY "Transfer money is selected."
-           DISPLAY "----------------------"
+           DISPLAY "----------------------------"
+
+           DISPLAY "Enter User ID: "
+           ACCEPT WS-USER-ID.
+           CALL "SHOW_USER" USING WS-USER-ID IS-VALID
+           DISPLAY "Who do yu want to transfer: "
+           ACCEPT TRANSFER-TO-ID.
+           CALL "SHOW_USER" USING TRANSFER-TO-ID IS-VALID
+           *> how much money do you want to trasfer
+           DISPLAY "How much money do you want to trasfer?"
+           ACCEPT TRANSFER-AMOUNT
+           *> Validation
+           OPEN I-O BALANCE-DB-FILE.
+           MOVE WS-USER-ID TO BALANCE-REC-ID
+           READ BALANCE-DB-FILE KEY IS BALANCE-REC-ID
+              INVALID
+                 DISPLAY "ERROR INVALID KEY"
+              NOT INVALID KEY
+                 IF BALANCE-REC-BALANCE > TRANSFER-AMOUNT
+                    MOVE 1 TO DOES-HAVE-THE-MONEY 
+                    COMPUTE BALANCE-REC-BALANCE = BALANCE-REC-BALANCE - TRANSFER-AMOUNT 
+                    REWRITE BALANCE-DB-RECORD
+                 END-IF
+           END-READ
+           IF DOES-HAVE-THE-MONEY = 1
+                MOVE TRANSFER-TO-ID TO BALANCE-REC-ID
+                    READ BALANCE-DB-FILE KEY IS BALANCE-REC-ID
+                    NOT INVALID
+                          COMPUTE BALANCE-REC-BALANCE = BALANCE-REC-BALANCE + TRANSFER-AMOUNT 
+                          REWRITE BALANCE-DB-RECORD
+                    END-READ
+           END-IF   
+           
+           CLOSE BALANCE-DB-FILE.
+
+           CALL "SHOW_USERS"
+
+
+           *> Are you sure?
+           *> transfer the money
            GOBACK.
